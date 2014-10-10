a6cc36fee41228761e1ab261252a6cd633bd0bb3
chris-maroudas <mallibu97@gmail.com>
2014-10-11 02:32:36 +0300

    create movie model with basic validations

---
+++ app/models/movie.rb
@@ -0,0 +1,43 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  description :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
+class Movie < ActiveRecord::Base
+
+  # Validations
+  validates :title,
+            presence: true,
+            length: (1..255)
+
+  validates :description,
+            presence: true
+
+  validate :check_description_word_count
+
+  # Callbacks
+  before_validation :strip_empty_space
+
+  # Methods
+  private
+
+  # Remove whitespace from input
+  def strip_empty_space
+    self.title = title.strip unless title.blank?
+    self.description = description.strip unless description.blank?
+  end
+
+  # Description should be 2 or more words
+  def check_description_word_count
+    unless description.blank?
+      errors.add(:description, "Description should be at least 2 words long.") if description.split(" ").count < 2
+    end
+  end
+
+end
