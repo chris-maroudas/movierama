d27a5b602d26ef57d668a3776cc96899321f85b3
chris-maroudas <mallibu97@gmail.com>
2014-10-13 05:44:24 +0300

    Add session controller and helper module with signin/out functionality

---
+++ spec/features/authentication_pages_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "Authentication" do
+
+  subject { page }
+
+  describe "signin page" do
+    before { visit signin_path }
+
+    it { should have_button('Sign in') }
+  end
+
+  describe "signin" do
+
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_content('Wrong email and password combination') }
+    end
+
+
+  end
+end
