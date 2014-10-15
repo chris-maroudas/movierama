c8ed78de9f4a619283b0b4943ba9aaa0c5ba98b4
chris-maroudas <mallibu97@gmail.com>
2014-10-15 23:53:24 +0300

    Add authorization to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:show, :edit, :movies]
+  before_filter :check_if_user_is_signed, only: [:edit]
+
   def new
     @user = User.new
   end
@@ -21,10 +25,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def movies
+    @movies = @user.movies
+    @movies = params[:sort].present? ? @movies.custom_order(params[:by]) : @movies.custom_order("created_at")
+    render 'movies/index'
+  end
+
   private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def check_if_user_is_signed
+      redirect_to signin_url, notice: "You must be logged in to do that" unless signed_in?
     end
 
 end
