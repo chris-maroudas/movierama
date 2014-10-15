3a89e4c7a145469143065ab432636fd787eed436
chris-maroudas <mallibu97@gmail.com>
2014-10-15 23:54:18 +0300

    Refactor complex views to partials and helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def rating_counts_pluralizer(movie, value)
+    if value.in? %w(like hate)
+      pluralize(movie.send("#{value.pluralize}_count"), value)
+    end
+  end
+
+  def link_to_undo(rating)
+    content = content_tag(:p, class: "delete-wrapper") do
+      raw("You #{rating.positive? ? "like" : "hate"} this movie | " +
+      (link_to("Un#{rating.positive? ? "like" : "hate" }", rating, method: :delete)))
+    end
+  end
+
 end
