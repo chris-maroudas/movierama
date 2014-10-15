0d430d7fd2fb88ea27cacae1195ac0d738722c36
chris-maroudas <mallibu97@gmail.com>
2014-10-15 23:52:48 +0300

    Remove unneeded code from movies_controller and add authorization

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,15 @@
 class MoviesController < ApplicationController
 
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
+  before_filter :check_if_user_is_signed, only: [:new, :create, :update, :destroy]
 
   def index
-    @movies = Movie.all
+    @movies = Movie.all.includes(:user)
+    @movies = params[:sort].present? ? @movies.custom_order(params[:by]) : @movies.custom_order("created_at")
   end
 
   def show
+    @rating = Rating.find_or_initialize_by(user: current_user, movie: @movie)
   end
 
   def new
@@ -17,46 +20,38 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.new(movie_params)
-
-    respond_to do |format|
-      if @movie.save
-        format.html { redirect_to @movie, notice: 'Movie was successfully created.' }
-        format.json { render :show, status: :created, location: @movie }
-      else
-        format.html { render :new }
-        format.json { render json: @movie.errors, status: :unprocessable_entity }
-      end
+    @movie = current_user.movies.new(movie_params)
+    if @movie.save
+      redirect_to root_url, notice: 'Movie was successfully created.'
+    else
+      render :new
     end
   end
 
   def update
-    respond_to do |format|
-      if @movie.update(movie_params)
-        format.html { redirect_to @movie, notice: 'Movie was successfully updated.' }
-        format.json { render :show, status: :ok, location: @movie }
-      else
-        format.html { render :edit }
-        format.json { render json: @movie.errors, status: :unprocessable_entity }
-      end
+    if @movie.update(movie_params)
+      redirect_to root_url, notice: 'Movie was successfully updated.'
+    else
+      render :edit
     end
   end
 
   def destroy
     @movie.destroy
-    respond_to do |format|
-      format.html { redirect_to movies_url, notice: 'Movie was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to movies_url, notice: 'Movie was successfully destroyed.'
   end
 
   private
-  def set_movie
-    @movie = Movie.find(params[:id])
-  end
+    def set_movie
+      @movie = Movie.find(params[:id])
+    end
 
-  def movie_params
-    params.require(:movie).permit(:title, :description)
-  end
+    def movie_params
+      params.require(:movie).permit(:title, :description)
+    end
+
+    def check_if_user_is_signed
+      redirect_to signin_url, notice: "You must be logged in to do that" unless signed_in?
+    end
 
 end
