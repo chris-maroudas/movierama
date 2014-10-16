ca53df35ffe69d9b8bb7c79257a1df4b99e3a699
chris-maroudas <mallibu97@gmail.com>
2014-10-16 03:59:49 +0300

    Fix broken rating spec

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -18,7 +18,7 @@ describe Rating do
   before do
     user = User.create(name: "Christos", email: "test@gmail.com", password: "12345678", password_confirmation: "12345678")
     another_user = User.create(name: "George", email: "test2@gmail.com", password: "12345678", password_confirmation: "12345678")
-    movie = Movie.create(title: "Sherlock Holmes", description: "A mystery film", user: another_user)
+    movie = Movie.create(title: "Sherlock Holmes", description: "A mystery film", user: another_user, published_at: Date.today)
     @rating = Rating.create(user: user, movie: movie, positive: true)
   end
 
